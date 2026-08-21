f62ac3fa9a2c00131d806dec32b883a3e0bf251b
Edwin <edwin.rubio@toshiba.com>
2026-08-21 12:10:32 -0500

    Completed the project.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import turtle, pandas
+import turtle, pandas, csv
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -11,11 +11,15 @@ turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 
 correct_states = []
+states2learn = []
 
-while True:
-    states = data.state # Or do all_states = data.state.to_list()
+states = data.state # Or do all_states = data.state.to_list()
+
+while len(correct_states) < 50:
     answer_state = screen.textinput(f"{len(correct_states)}/{len(states.values)} States Correct", "What's another state's name?").title()
 
+    if answer_state == "Exit":
+        break
     if answer_state in states.values:
         state = turtle.Turtle()
         state.hideturtle()
@@ -25,3 +29,9 @@ while True:
         state.goto(x_cor, y_cor)
         state.write(answer_state, font=("Arial", 12, "normal"))
         correct_states.append(answer_state)
+
+for state in states.values:
+    if state not in correct_states:
+        states2learn.append(state)
+
+pandas.Series(states2learn).to_csv('states_to_learn.csv', header=False, sep=" ", lineterminator="\n")
